6a51973a1bfbb895cb75ff6f852c2a61238ca60f
Tsukasa Arima <pyar6329@gmail.com>
2025-08-01 01:48:27 +0000

    fix codes

--- cmd/main.go
+++ cmd/main.go
@@ -203,8 +203,9 @@ func main() {
 	}
 
 	if err := (&controller.SecurityCheckReconciler{
-		Client: mgr.GetClient(),
-		Scheme: mgr.GetScheme(),
+		Client:   mgr.GetClient(),
+		Scheme:   mgr.GetScheme(),
+		Recorder: mgr.GetEventRecorderFor("securitycheck-controller"),
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "SecurityCheck")
 		os.Exit(1)
--- internal/controller/securitycheck_controller.go
+++ internal/controller/securitycheck_controller.go
@@ -43,6 +43,8 @@ type SecurityCheckReconciler struct {
 // +kubebuilder:rbac:groups=security.k8s-operator.pyar.bz,resources=securitychecks,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=security.k8s-operator.pyar.bz,resources=securitychecks/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=security.k8s-operator.pyar.bz,resources=securitychecks/finalizers,verbs=update
+// +kubebuilder:rbac:groups="",resources=pods,verbs=get;list;watch
+// +kubebuilder:rbac:groups="",resources=events,verbs=create;patch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
